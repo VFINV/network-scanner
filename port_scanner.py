45e36c2d15412a823dff5477688e6e4699c1ee27
VFINV <vfinv@outlook.com>
2025-05-31 20:26:59 +0800

    Update port_scanner.py

--- port_scanner.py
+++ port_scanner.py
@@ -1,16 +1,19 @@
 import socket
 
-target = input("Enter target IP: ")
-print(f"\n Scanning target {target}...\n")
+target = input("Enter target IP or hostname: ")
 
-for port in range(20, 102): 
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    socket.setdefaulttimeout(1)
+try:
+    target_ip = socket.gethostbyname(target)
+    print(f"\nScanning target {target_ip}...\n")
+except socket.gaierror:
+    print("Invalid hostname or IP")
+    exit()
 
-    result = sock.connect_ex((target, port))
-    if result == 0:
-        print(f"Port {port} is open")
-    sock.close()
+for port in range(20, 102):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        socket.setdefaulttimeout(1)
+        result = sock.connect_ex((target_ip, port))
+        if result == 0:
+            print(f"Port {port} is open")
 
 print("Scan complete")
-#test
